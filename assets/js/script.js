acd82865b1915c493adae2bb738210e5dd981688
danRdS <danielrdsilva@gmail.com>
2024-02-01 20:36:27 -0300

    teste 6 orientacao imagens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,185 +1,3 @@
-// const modalFormUpload = document.querySelector('.modal-form-upload');
-// const uploadForm = document.getElementById('uploadForm');
-// const btnIniciarAddFoto = document.getElementById('btn-iniciar-add-foto');
-
-// const abrirAreaSelecaoImagens = () => {
-//     btnIniciarAddFoto.classList.add('botao-add-foto-clicado');
-//     btnIniciarAddFoto.classList.remove('botao-add-foto-retira-animacao');
-
-//     setTimeout(() => {
-//         modalFormUpload.classList.add('mostrar-popup-upload-imagem');
-//         uploadForm.classList.add('animacao-descendente-form');
-//     }, 600);
-// }
-
-// const cancelar = () => {
-//     modalFormUpload.classList.remove('mostrar-popup-upload-imagem');
-//     uploadForm.classList.remove('animacao-descendente-form');
-//     areaFotosPreSelecionadas.classList.add('ocultar');
-//     previewContainer.innerHTML = '';
-//     totalInicialDeFotos.textContent = '';
-
-//     setTimeout(() => {
-//         btnIniciarAddFoto.classList.remove('botao-add-foto-clicado');
-//         btnIniciarAddFoto.classList.add('botao-add-foto-retira-animacao');
-//     }, 100)
-// }
-
-// const areaFotosPreSelecionadas = document.querySelector('.area-fotos-pre-selecionadas');
-// const totalInicialDeFotos = document.querySelector('.total-inicial');
-// const previewContainer = document.getElementById('previewContainer');
-// const viewContainer = document.getElementById('viewContainer');
-// const buttonConfirmar = document.getElementById('confirmar');
-
-// let urlsImagens = [];
-
-// function uploadFiles() {
-//     const fileInput = document.getElementById('fileInput');
-//     const files = fileInput.files;
-//     totalInicialDeFotos.textContent = '';
-
-//     if (files.length === 0) {
-//         alert('Por favor, selecione pelo menos um arquivo.');
-//         return;
-//     }
-    
-//     if (files.length > 7) {
-//       alert('Você só pode enviar até 7 imagens.');
-//       buttonConfirmar.setAttribute('disabled', 'disabled');
-//       return;
-//     }
-  
-//     totalInicialDeFotos.textContent = files.length;
-//     previewContainer.innerHTML = '';
-//     urlsImagens = [];
-//     areaFotosPreSelecionadas.classList.remove('ocultar');
-
-//     for (let i = 0; i < files.length; i++) {
-//       const file = files[i];
-//       const reader = new FileReader();
-  
-//       reader.onload = function (e) {
-//         const imgPreview = document.createElement('img');
-//         imgPreview.src = e.target.result;
-//         imgPreview.alt = `Imagem ${i + 1}`;
-//         previewContainer.appendChild(imgPreview);
-
-//         urlsImagens.push(imgPreview.src);
-//         buttonConfirmar.removeAttribute('disabled');
-//     };
-    
-//      reader.readAsDataURL(file);
-//     }
-// }
-
-// function abrirEscolhas() {
-//     previewContainer.innerHTML = '';
-//     fileInput.value = '';
-// }
-
-// function confirmarFotos() {
-//     areaFotosPreSelecionadas.classList.add('ocultar');
-//     buttonConfirmar.setAttribute('disabled', 'disabled');
-//     btnIniciarAddFoto.classList.remove('botao-add-foto-clicado');
-//     btnIniciarAddFoto.classList.add('botao-add-foto-retira-animacao');
-//     renderizarSlides(urlsImagens);
-
-//     localStorage.setItem('imagensSlide', JSON.stringify(urlsImagens));
-
-//     cancelar();
-// }
-
-// // Area dos slides
-// const slides = document.querySelectorAll('.slide');
-// const indiceImagem = document.getElementById('indiceImagem');
-// const imagens = document.querySelector('.imagens');
-// let contador = 0;
-
-// function renderizarSlides(listaImagens) {
-//     imagens.innerHTML = '';
-//     contador = 0;
-
-//     listaImagens.forEach((slide, index) => {
-//         const imagem = document.createElement('img');
-//         imagem.src = listaImagens[index];
-//         if(urlsImagens.length) {
-//             imagem.src = urlsImagens[index];
-//         }
-//         imagem.alt = `Imagem ${index + 1}`;
-//         imagens.appendChild(imagem);
-//         imagem.style.left = `${index * 100}%`;
-//     })
-//     indiceImagem.innerText = `${contador + 1}/${imagens.children.length}`;
-// }
-
-// // area localStorage
-// const imagensNoLocalStorage = [];
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     if(localStorage.getItem('imagensSlide')) {
-//         const arrayRecuperado = JSON.parse(localStorage.getItem('imagensSlide'));
-//         arrayRecuperado.forEach(imagem => {
-//             imagensNoLocalStorage.push(imagem);
-//         })
-    
-//         renderizarSlides(imagensNoLocalStorage);
-//     }
-// });
-
-// const verAnterior = () => {
-//     if(!contador) {
-//         contador = imagens.children.length - 1;
-//         slideImage();
-//     } else {
-//         contador--;
-//         slideImage();
-//     }
-// };
-
-// const verProximo = () => {
-//     if(contador == imagens.children.length - 1) {
-//         contador = 0;
-//         slideImage();
-//     } else {
-//         contador++;
-//         slideImage();
-//     }
-// };
-
-// const slideImage = () => {
-//     Object.values(imagens.children).forEach(slide => {
-//         slide.style.transform = `translateX(-${contador * 100}%)`;
-//         indiceImagem.innerText = `${contador + 1}/${imagens.children.length}`;
-//     })
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const modalFormUpload = document.querySelector('.modal-form-upload');
 const uploadForm = document.getElementById('uploadForm');
 const btnIniciarAddFoto = document.getElementById('btn-iniciar-add-foto');
@@ -280,28 +98,28 @@ async function getImageDimensions(base64) {
     });
 }
 
-async function compressImage(file) {
-    return new Promise((resolve, reject) => {
-        const options = {
-            quality: .5, // Ajuste a qualidade conforme necessário
-            // maxWidth: 800, // Largura máxima da imagem
-            // maxHeight: 600, // Altura máxima da imagem
-        };
+// async function compressImage(file) {
+//     return new Promise((resolve, reject) => {
+//         const options = {
+//             quality: .5, // Ajuste a qualidade conforme necessário
+//             // maxWidth: 800, // Largura máxima da imagem
+//             // maxHeight: 600, // Altura máxima da imagem
+//         };
 
-        new ImageCompressor(file, {
-            ...options,
-            success(result) {
-                const reader = new FileReader();
-                reader.readAsDataURL(result);
-                reader.onload = () => resolve(reader.result);
-            },
-            error(error) {
-                reject(error);
-            },
-        });
+//         new ImageCompressor(file, {
+//             ...options,
+//             success(result) {
+//                 const reader = new FileReader();
+//                 reader.readAsDataURL(result);
+//                 reader.onload = () => resolve(reader.result);
+//             },
+//             error(error) {
+//                 reject(error);
+//             },
+//         });
 
-    });
-}
+//     });
+// }
 
 
 
@@ -349,42 +167,63 @@ async function compressImage(file) {
 
 
 
+async function compressImage(file) {
+    return new Promise((resolve, reject) => {
+        const options = {
+            quality: 0.5,
+            // maxWidth: 800,
+            // maxHeight: 600,
+        };
+
+        const reader = new FileReader();
+        reader.onload = async (e) => {
+            const base64 = e.target.result;
+
+            const img = new Image();
+            img.onload = () => {
+                const canvas = document.createElement('canvas');
+                const ctx = canvas.getContext('2d');
+
+                const { width, height } = img;
+
+                canvas.width = width;
+                canvas.height = height;
+
+                ctx.drawImage(img, 0, 0, width, height);
+
+                const correctedBase64 = canvas.toDataURL('image/jpeg', options.quality);
+                resolve(correctedBase64);
+            };
+
+            img.src = base64;
+        };
+
+        reader.onerror = reject;
+        reader.readAsDataURL(file);
+    });
+}
+
+
+
+
 // async function compressImage(file) {
 //     return new Promise((resolve, reject) => {
 //         const options = {
-//             quality: 0.4,
-//             // maxWidth: 800,
-//             // maxHeight: 600,
+//             canvas: true,
+//             orientation: true,
+//             maxWidth: 800,
+//             maxHeight: 600,
 //         };
 
-//         const reader = new FileReader();
-//         reader.onload = async (e) => {
-//             const base64 = e.target.result;
-
-//             const img = new Image();
-//             img.onload = () => {
-//                 const canvas = document.createElement('canvas');
-//                 const ctx = canvas.getContext('2d');
-
-//                 const { width, height } = img;
-
-//                 canvas.width = width;
-//                 canvas.height = height;
-
-//                 ctx.drawImage(img, 0, 0, width, height);
-
-//                 const correctedBase64 = canvas.toDataURL('image/jpeg', options.quality);
-//                 resolve(correctedBase64);
-//             };
-
-//             img.src = base64;
-//         };
-
-//         reader.onerror = reject;
-//         reader.readAsDataURL(file);
+//         loadImage(file, (canvas) => {
+//             canvas.toBlob((blob) => {
+//                 const reader = new FileReader();
+//                 reader.onload = () => resolve(reader.result);
+//                 reader.readAsDataURL(blob);
+//             }, 'image/jpeg');
+//         }, options);
 //     });
 // }
-
 
 
 
